f69a366793240079661a68aa9aa072a69bf6a7fb
s18964 <s18964@LAB001-14>
2020-03-09 13:43:56 +0100

    Init

---
+++ Cw2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cw2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parsedDate = DateTime.Parse("2020-03-09");
+            var today = DateTime.Today;
+            Console.WriteLine(parsedDate);
+            Console.WriteLine(today.ToShortDateString());
+        }
+    }
+}
---
+++ Cw2/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cw2
+{
+    public class Student
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public string kierunek { get; set; }
+        public string tryb { get; set; }
+        public int index { get; set; }
+        public string dataur { get; set; }
+        public string email { get; set; }
+        public string mama { get; set; }
+        public string tata { get; set; }
+
+
+
+    }
+}
